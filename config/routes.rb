9112aa4ec00c3975a34c8bb5f34056fef5b9a3ed
Rob Merrill <robgmerrill@gmail.com>
2019-04-15 20:04:05 -0700

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  
   # maps requests for the URL /static_pages/home to the home action in
   # the Static Pages controller
   get 'static_pages/home'
@@ -9,6 +12,5 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+
 end
